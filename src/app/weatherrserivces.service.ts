41beb7f1189c67d5de3ede6ab355c140c808fb7c
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-08-03 23:18:35 +0530

    task

--- src/app/weatherrserivces.service.ts
+++ src/app/weatherrserivces.service.ts
@@ -11,7 +11,8 @@ export class WeatherrserivcesService {
   private apiKey = 'd4594364698122bfd1c4b3eb5f2ff19f';
   private historyApiUrl = 'https://history.openweathermap.org/data/2.5/history/city';
 
- 
+  private forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+
   
 
   constructor(
@@ -23,8 +24,13 @@ export class WeatherrserivcesService {
   getWeather(city: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}?q=${city}&appid=${this.apiKey}`);
   }
-  getHistory(lat: number, lon: number): Observable<any> {
-    return this.http.get<any>(`${this.historyApiUrl}?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
+  // getForecast(cityId: number): Observable<any> {
+  //   const startDate = Math.floor(Date.now() / 1000) - 7 * 24 * 3600; // 7 days ago
+  //   const endDate = Math.floor(Date.now() / 1000); // now
+  //   return this.http.get(`${this.forecastUrl}?id=${cityId}&type=hour&start=${startDate}&end=${endDate}&appid=${this.apiKey}`);
+  // }
+  getForecast(cityId: number): Observable<any> {
+    return this.http.get<any>(`${this.forecastUrl}?id=${cityId}&appid=${this.apiKey}&units=metric`);
   }
 
   private counterCount = new BehaviorSubject<number>(0);
--- src/app/wheater-app/wheater-app.component.ts
+++ src/app/wheater-app/wheater-app.component.ts
@@ -1,73 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherrserivcesService } from '../weatherrserivces.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-wheater-app',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   providers: [WeatherrserivcesService, HttpClient],
   templateUrl: './wheater-app.component.html',
-  styleUrl: './wheater-app.component.css'
+  styleUrls: ['./wheater-app.component.css']
 })
-export class WheaterAppComponent implements OnInit{
-  city: string = '';
-  recentLocations: { city: string, temperature: number, weather: string, icon: string }[] = [];
+export class WheaterAppComponent implements OnInit {
+  cityName: string = '';
+  cities: { id: number, name: string, temperature: number, icon: string }[] = [];
+  forecast: any;
   errorMessage: string = '';
-  historyData:any
-  constructor(public http:WeatherrserivcesService){}
-  ngOnInit(){
+
+  constructor(public http: WeatherrserivcesService) { }
+
+  ngOnInit() {
+    // Initial load logic if needed
   }
 
   addCity() {
-    this.http.getWeather(this.city).subscribe({
-      next: (data: any) => {
-        console.log(data)
-        if (data) {
-          this.recentLocations.unshift({
-            city: this.city,
-            temperature: data.main.temp,
-            weather: data.weather[0].description,
-            icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-          });
-          this.city = '';
-          this.errorMessage = '';
+    this.http.getWeather(this.cityName).subscribe({
+      next: data => {
+        const city = {
+          id: data.id,
+          name: this.cityName,
+          temperature: data.main.temp,
+          icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        };
+        if (this.cities.length >= 8) {
+          this.cities.pop();
         }
+        this.cities.unshift(city);
+        this.cityName = '';
+        this.errorMessage = '';
       },
       error: (error: HttpErrorResponse) => {
         this.errorMessage = 'City not found';
       }
-    }
-    );
+    });
   }
 
-  refreshWeather(location: { city: string }) {
-    this.http.getWeather(location.city).subscribe(
-      data => {
-        if (data) {
-          console.log(data)
-          const index = this.recentLocations.findIndex(loc => loc.city === location.city);
-          this.recentLocations[index] = {
-            city: location.city,
-            temperature: data.main.temp,
-            weather: data.weather[0].description,
-            icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-          };
-        }
+  refreshCity(city: any, event: Event) {
+    event.stopPropagation();
+    this.http.getWeather(city.name).subscribe({
+      next: data => {
+        city.temperature = data.main.temp;
+        city.icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert('Failed to refresh city');
       }
-    );
+    });
   }
 
-  removeLocation(location: { city: string }) {
-    this.recentLocations = this.recentLocations.filter(loc => loc.city !== location.city);
+  removeCity(city: any, event: Event) {
+    event.stopPropagation();
+    this.cities = this.cities.filter(c => c !== city);
   }
 
-  getHistory(lat: number, lon: number): void {
-    this.http.getHistory(lat, lon).subscribe(data => {
-      this.historyData = data;
-      console.log(this.historyData)
+  clearCities() {
+    this.cities = [];
+    this.forecast = null;
+  }
+
+  selectCity(city: any) {
+    this.http.getForecast(city.id).subscribe({
+      next: data => {
+        this.forecast = data;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert('Failed to load forecast');
+      }
     });
   }
 }
