d1f7af1d2a1cf200d64ee3843e969e5cecf1998f
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-08-21 17:20:15 +0530

    task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule,HeaderComponent,CommonModule],
+  imports: [RouterOutlet,HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
